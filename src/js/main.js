6dff6a851f9b6363ca1a2505ee34fcea2d84f852
Marcos <uchodc@gmail.com>
2018-02-20 19:43:55 +0000

    Even more layout fun!

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,27 @@
 import $ from 'jquery'
 import './modernizr-custom.min'
+
+// IMAGES
+
+const getMaterialImg = material => ({
+  cocktail: `${cocktail}`,
+  cloth: `${cloth}`,
+  coffeemachine: `${coffeemachine}`,
+  food: `${food}`,
+  botijo: `${botijo}`,
+  guitar: `${guitar}`,
+  hoe: `${hoe}`,
+  keyboard: `${keyboard}`,
+  microphone: `${microphone}`,
+  monster: `${monster}`,
+  musicsheet: `${musicsheet}`,
+  potatoes: `${potatoes}`,
+  powerglove: `${powerglove}`,
+  stackoverflow: `${stackoverflow}`,
+  synth: `${synth}`,
+  tractor: `${tractor}`
+})[material]
+
+$(document).ready(() => {
+
+})
