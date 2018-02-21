0213629d6fb7dee3bc6689f4a1e414241d54f9bb
Marcos <uchodc@gmail.com>
2018-02-21 01:01:44 +0000

    Dark Teatime of the Soul

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import $ from 'jquery'
 import './modernizr-custom.min'
+import svg from './svglib'
 
 // IMAGES
 import audio1 from '../images/audio1.jpg'
@@ -7,6 +8,11 @@ import audio2 from '../images/audio2.jpg'
 import audio3 from '../images/audio3.jpg'
 
 const playlist = ['audio1', 'video1', 'audio2', 'video2', 'audio3', 'video3']
+const playlistLength = playlist.length
+
+let counter
+let playpauseStatus = 'play'
+
 
 const getAudioImg = audio => ({
   audio1: `${audio1}`,
@@ -21,11 +27,95 @@ const getAudioSrc = audio => ({
 })[audio]
 
 const getVideoSrc = video => ({
-  video1: 'js/video/video1',
-  video2: 'js/video/video2',
-  video3: 'js/video/video3',
+  video1: 'js/video/video1.mp4',
+  video2: 'js/video/video2.mp4',
+  video3: 'js/video/video3.mp4'
 })[video]
 
-$(document).ready(() => {
+function nextTrack() {
+  $('video').remove()
+  $('audio').removeAttr('src')
+  $('video').removeClass('playing')
+  $('audio').removeClass('playing')
+  $('li').removeClass('played')
+  
+  $('.screen').css('background-image', '')
 
+  if (counter !== 0) {
+    const oldTrack = playlist[counter - 1]
+    $(`#${oldTrack}`).closest('li').removeClass('played')
+  }
+  
+  counter = (counter === playlistLength) ? 0 : counter
+  const newTrack = playlist[counter]
+
+  if (newTrack.substring(0, 5) === 'audio') {
+    $('audio').attr('src', getAudioSrc(newTrack))
+    $(`#${newTrack}`).closest('li').addClass('played')
+    $('audio').addClass('playing')
+    $('.screen').css('background-image', `url(${getAudioImg(newTrack)})`)
+    $('audio')[0].play()
+  } else {
+    $('audio')[0].pause()
+    $('.screen').append('<video class="playing"></video>')
+    $('video').attr('src', getVideoSrc(newTrack))
+    $('video').on('ended', () => {
+      counter += 1
+      nextTrack()
+    })
+    $(`#${newTrack}`).closest('li').addClass('played')
+    $('video')[0].play()
+  }
+}
+
+function addAVEvents() {
+  $('audio').on('ended', () => {
+    counter += 1
+    nextTrack()
+  })
+  $('video').on('ended', () => {
+    counter += 1
+    nextTrack()
+  })
+}
+
+function activatePlaylist() {
+  $('li p').click((event) => {
+    const media = event.currentTarget.id
+    for (let i = 0; i < playlistLength; i += 1) {
+      if (playlist[i] === media) {
+        counter = i
+      }
+    }
+    nextTrack()
+  })
+}
+
+function activateControls() {
+  $('#playpause').click(() => {
+    $('#playpause').empty()
+    if (playpauseStatus === 'play') {
+      playpauseStatus = 'pause'
+      $('.playing')[0].pause()
+      $('#playpause').append($(svg.lib.play))
+    } else {
+      playpauseStatus = 'play'
+      $('.playing')[0].play()
+      $('#playpause').append($(svg.lib.pause))
+    }
+  })
+  $('#backward').click(() => {
+    $('.playing')[0].currentTime -= 10
+  })
+  $('#forward').click(() => {
+    $('.playing')[0].currentTime += 10
+  })
+}
+
+$(document).ready(() => {
+  counter = 0
+  activateControls()
+  activatePlaylist()
+  addAVEvents()
+  nextTrack()
 })
